a150e7aa1987772ba9a3d9cf2ec67c12e24c3beb
Eyüp <89838363+Eyyubi26@users.noreply.github.com>
2023-03-09 22:22:20 +0300

    How to setup "openai" library
    1- Open your terminal. (CMD, Powershell etc.)
    2- "pip install openai" (If python is not installed you need to install python).
    3- Write your codeeee.

---
+++ ai.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "YOUR_OPENAI_API_KEY"
+
+model_engine = "text-davinci-003"
+prompt = input('Your question: ')
+
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=3000,
+    n=1,
+    stop=None,
+    temperature=1,
+)
+
+response = completion.choices[0].text
+print(response)
